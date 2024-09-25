77136339163a04d615c1e25cfa16891a7113b01e
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-25 20:28:02 +0800

    added array and object using template letaral string

--- template.js
+++ template.js
@@ -1,20 +1,44 @@
-const jscpoint = 3.45;
-const sscPoint = 4.93;
+// const jscpoint = 3.45;
+// const sscPoint = 4.93;
 
-const myGpa = `
-   jsc point: ${jscpoint} 
-   ssc point: ${sscPoint}
-`;
-console.log(myGpa);
+// const myGpa = `
+//    jsc point: ${jscpoint} 
+//    ssc point: ${sscPoint}
+// `;
+// console.log(myGpa);
 
 
 // check arrar with backtic sign and change the array value
-const numbers = [12, 43, 67, 67];
+// const numbers = [12, 43, 67, 67];
 
-const showNumberValue = `
-   first Number is = ${numbers[0]}
-   second Number is = ${numbers[1]}
-   third number is = ${numbers[2]}
-   new fourth number is = ${numbers[3] = 5}
-`;
-console.log(showNumberValue);
+// const showNumberValue = `
+//    first Number is = ${numbers[0]}
+//    second Number is = ${numbers[1]}
+//    third number is = ${numbers[2]}
+//    new fourth number is = ${numbers[3] = 5}
+// `;
+// console.log(showNumberValue);
+
+
+const fruits = {
+    bananaPrice: 45,
+    orangePrice: 50,
+    fruitsName:{
+        fName: 'strobary',
+        sName: 'dragon',
+        tName: 'mango'
+    }
+}
+
+const fruitsDetails = `
+    Total fruit cost list:  
+    1. Banana Price is ${45} 
+    2. Orange Price is ${50}
+    2. Total Fruit Price is ${fruits.bananaPrice + fruits.orangePrice}
+    
+    Which fruit i buy list:
+    1. very sweet fruit ${fruits.fruitsName.fName}
+    2. most color fun and little sweet fruit ${fruits.fruitsName.sName = 'pynapple'}
+    3. my most favourit fruit ${fruits.fruitsName.tName}
+`
+console.log(fruitsDetails);
