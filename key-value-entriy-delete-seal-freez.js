c13191386dd03df68d7c5819d79f3e8aa09fb0ef
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-26 01:14:01 +0800

    added key value delete seal freez entryi

---
+++ key-value-entriy-delete-seal-freez.js
@@ -0,0 +1,42 @@
+// const cars = {
+//     fcarName: 'BMW',
+//     scarName: 'Bugadi',
+//     tcarName: 'lamborginni',
+//     fthcarName: 'mazda'
+// }
+
+// const keys = Object.keys(cars);
+// const values = Object.values(cars);
+// // console.log(keys);
+// // console.log(...values);
+
+// // return the value is array of array 
+// // [
+// //   [ 'fcarName', 'BMW' ],
+// //   [ 'scarName', 'Bugadi' ],
+// //   [ 'tcarName', 'lamborginni' ],
+// //   [ 'fthcarName', 'mazda' ]
+// // ]
+// const entryies = Object.entries(cars);
+// // console.log(entryies);
+
+// // delete object key values !
+// // delete cars.tcarName;
+// // console.log(cars);
+
+// // distructuring delete
+
+// const {fcarName, ...rest} = cars;
+// console.log(rest);
+
+// const freez = Object.freeze(cars);
+// cars.fcarName = 'kwasaki';
+// cars.needBike = 'yamaha 150cc bike'
+// // console.log(cars);
+
+// // seal 
+
+// const seal = Object.seal(cars);
+// // console.log(seal);
+
+
