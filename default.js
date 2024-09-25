179c8dbec14495779da0c4f2b054806594d01790
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-25 20:07:56 +0800

    first commit

---
+++ default.js
@@ -0,0 +1,55 @@
+// without argument
+
+// function numbersAdd(num1, num2){
+//     const result = num1 + num2;
+//     console.log(num1 , num2, result);
+//     return result;
+
+// }
+
+// numbersAdd();
+
+// function numbersAdd(num1, num2){
+//     const result = num1 + num2;
+//     console.log(num1 , num2, result);
+//     return result;
+
+// }
+
+// numbersAdd(50);
+
+// function numbersAdd(num1, num2){
+//     const result = num1 + num2;
+//     console.log(num1 , num2, result);
+//     return result;
+
+// }
+
+// numbersAdd(40, 60);
+
+// function numbersAdd(num1 = 1, num2 = 0){
+//     const result = num1 + num2;
+//     console.log(num1 , num2, result);
+//     return result;
+
+// }
+
+// numbersAdd();
+
+// function numbersAdd(num1, num2 = 0){
+//     const result = num1 + num2;
+//     console.log(num1 , num2, result);
+//     return result;
+
+// }
+
+// numbersAdd(50);
+
+// function numbersAdd(num1 = 0, num2 = 0){
+//     const result = num1 + num2;
+//     console.log(num1 , num2, result);
+//     return result;
+
+// }
+
+// numbersAdd();
