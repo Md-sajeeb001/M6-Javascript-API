f583dc0f3e5bd98009e3aa0af79623e9797fa340
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-25 23:33:08 +0800

    added spreed operator advanced use case

--- spreed.js
+++ spreed.js
@@ -11,13 +11,21 @@
 
 
 // using spreed operator and 
-const numbers = [32, 44, 12, 55, 45, 89];
+// const numbers = [32, 44, 12, 55, 45, 89];
 
-// const number = Math.min(...numbers);
-const number = numbers;
-console.log(number)
-number.push(11, 111);
+// // const number = Math.min(...numbers);
+// const number = numbers;
+// console.log(number)
+// number.push(11, 111);
 
-let changeValue = number;
-changeValue = [88, 77, 66, 55];
-console.log(...changeValue);
+// let changeValue = number;
+// changeValue = [88, 77, 66, 55];
+// console.log(...changeValue);
+
+const arrayOfNumber = [55, 88, 102, 45, 76];
+const maxNum = Math.max(...arrayOfNumber);
+
+// Advanced 
+const addNum = [...arrayOfNumber, 500, 600];
+console.log(maxNum);
+console.log(...addNum);
