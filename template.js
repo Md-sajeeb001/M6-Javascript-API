1834df680171d9ed233bc0d44b3228ba117588fd
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-25 20:15:57 +0800

    added template string

---
+++ template.js
@@ -0,0 +1,20 @@
+const jscpoint = 3.45;
+const sscPoint = 4.93;
+
+const myGpa = `
+   jsc point: ${jscpoint} 
+   ssc point: ${sscPoint}
+`;
+console.log(myGpa);
+
+
+// check arrar with backtic sign and change the array value
+const numbers = [12, 43, 67, 67];
+
+const showNumberValue = `
+   first Number is = ${numbers[0]}
+   second Number is = ${numbers[1]}
+   third number is = ${numbers[2]}
+   new fourth number is = ${numbers[3] = 5}
+`;
+console.log(showNumberValue);
