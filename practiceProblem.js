5b768a725384e97b9d7c2c93a6cb122b2bd31c96
Md-sajeeb001 <mohammadsajeeb890@email.com>
2024-09-26 19:28:54 +0800

    added practice task

--- practiceProblem.js
+++ practiceProblem.js
@@ -25,26 +25,51 @@
 
 // =========== Practice Problem 2 ============= //
 
-// 1. write an arrow function where it will do the following:
+// 2-1. write an arrow function where it will do the following:
 
 // a) it will take an array where the array elements will be the name of your friends
 
 // b) check it the length of each element is event, push elements with even length to a new array and return the result 
 
-const freshArray = [];
-const friends = (names) => {
-    for(const name of names){
-        const len = name.length;
-        if(len % 2 === 0){
-            // push event name
-            freshArray.push(name);
-            // push event name with event number
-            freshArray.push(len, name);
-        }
+// const freshArray = [];
+// const friends = (names) => {
+//     for(const name of names){
+//         const len = name.length;
+//         if(len % 2 === 0){
+//             // push event name
+//             freshArray.push(name);
+//             // push event name with event number
+//             freshArray.push(len, name);
+//         }
+//     }
+//     return freshArray;
+// }
+
+// const friendsName = ['abul', 'kabul', 'ahesan', 'shakil ahmed', 'babul hasan', 'sofiq ul'];
+// const friend = friends(friendsName);
+// console.log(freshArray);
+
+// =============== Practice Problem 3 ============== //
+
+// write an arrow function where it will do the following: x2
+
+// a) Square each array element
+// b) Calculate the sum of the squared elements
+// c) Return the average of the sum of the squared elements
+
+// let newArray = [];
+let sum = 0;
+const arrowFunction = (numbers) => {
+    for(const num of numbers){
+        const mulNum = num * num;
+        console.log(mulNum);
+        sum = sum + mulNum;
+        console.log(sum)
+        const averageNum = sum / mulNum;
+        console.log(averageNum.toFixed(2))
     }
-    return freshArray;
 }
 
-const friendsName = ['abul', 'kabul', 'ahesan', 'shakil ahmed', 'babul hasan', 'sofiq ul'];
-const friend = friends(friendsName);
-console.log(freshArray);
+const arrayOfNumbers = [4, 12, 30, 8, 7, 5]
+const nums = arrowFunction(arrayOfNumbers);
+// console.log(newArray)
